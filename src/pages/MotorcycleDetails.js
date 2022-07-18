2759adcec65776c7a69e136f6331014b62b9cd6d
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-18 20:44:54 +0545

    Add image and details for the bike

--- src/pages/MotorcycleDetails.js
+++ src/pages/MotorcycleDetails.js
@@ -6,6 +6,23 @@ const MotorcycleDetails = () => (
     <div className="flex  justify-center  w-full">
       <h1 className="font-semibold text-2xl">Kawasaki Ninja H2R</h1>
     </div>
+    <div className="flex flex-col  gap-[10%] items-center md:flex-row md:gap-[10%] md:items-center">
+      <div className="bg-amber-500 w-80 h-80 rounded-full mx-auto relative mb-4 flex justify-center items-center content-center md:bg-amber-500 md:w-[30rem] md:h-[15rem] md:rounded-full ">
+        <div className="flex justify-center items-center content-center">
+          <img className="w-[100%] h-[100%] md:w-[100%] md:h-[100%]" src={kawasaki} alt="img" />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-4">
+        <p className="w-[70%] ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, tempora eligendi! Nesciunt quibusdam autem, numquam velit quo aliquam, porro vero quae recusandae maiores error exercitationem quidem natus voluptatem ab placeat.</p>
+        <div className="w-[70%] ">
+          <p>Finance fee: $4000 </p>
+          <p>Option to Purchase fee: $20000</p>
+          <p>Total amount payable: $37000</p>
+          <p>Duration: 24 months</p>
+        </div>
+        
+      </div>
+    </div>
 </div>    
 );
 
