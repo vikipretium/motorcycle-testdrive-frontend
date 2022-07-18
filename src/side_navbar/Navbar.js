b9920c0a9dc11cf080e964eaaffed1fcaa1c819e
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-18 22:10:50 +0200

    Add Navbar links and add colours and transitions to them

--- src/side_navbar/Navbar.js
+++ src/side_navbar/Navbar.js
@@ -1,9 +1,20 @@
 function NavBar() {
   return (
-    <div className="bg-gradient-to-r from-cyan-300 to-sky-400 text-stone-800 w-1/6 space-y-4">
-      <p> Logo </p>
+    <div className="bg-gradient-to-r from-cyan-600 to-sky-200 text-stone-800 w-1/5 space-y-4 align-middle justify-center">
+      <p className="py-6"> Logo </p>
       <nav>
-        <a className="block py-3 5 px-5"> </a>
+        <a href="#home" className="block py-5 px-5 rounded transition duration 300 hover:bg-blue-500 hover:text-white">
+          <p> MODELS </p>
+        </a>
+        <a href="#home" className="block py-5 px-5 rounded transition duration 300 hover:bg-blue-500 hover:text-white">
+          <p>RESERVE </p>
+        </a>
+        <a href="#home" className="block py-5 px-5 rounded transition duration 300 hover:bg-blue-500 hover:text-white">
+          <p> MY RESERVATIONS </p>
+        </a>
+        <a href="#home" className="block py-5 px-5 rounded transition duration 300 hover:bg-blue-500 hover:text-white">
+          <p> SIGN OUT </p>
+        </a>
       </nav>
     </div>
   );
