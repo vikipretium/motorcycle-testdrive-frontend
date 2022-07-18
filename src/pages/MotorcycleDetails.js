c610ebbb97c3b58fef3b49b28b50b3cc26ac8a73
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-18 20:43:08 +0545

    Add h1 tag with the bike name

---
+++ src/pages/MotorcycleDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import kawasaki from 'images/kawasaki'
+
+const MotorcycleDetails = () => (
+    <div className="flex flex-col gap-[30%] p-2 items-center justify-center md:w-4/5">
+    <div className="flex  justify-center  w-full">
+      <h1 className="font-semibold text-2xl">Kawasaki Ninja H2R</h1>
+    </div>
+</div>    
+);
+
+export default MotorcycleDetails;
