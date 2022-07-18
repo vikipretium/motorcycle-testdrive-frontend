234ab718aa1d2fdd082f4ae0ed75dc4edbe68b5f
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-18 21:39:59 +0200

    Import NavBar component into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import NavBar from './side_navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Motocycle Book</h1>
+    <div className="App relative min-h-screen flex">
+      <NavBar />
+      <div>
+        {' '}
+        {/* Put all other content in this div  */}
+        <h1>Motocycle Book</h1>
+        Other Content Goes Here
+      </div>
     </div>
   );
 }
