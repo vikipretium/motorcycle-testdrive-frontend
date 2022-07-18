3c4c98a8e25d8a9c62f542d9f670851769ecdc7c
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-18 21:39:40 +0200

    Add basic structure of Nav
    - Add background colour
    - Create spacing for Nav Links
    - Add space for logo

---
+++ src/side_navbar/Navbar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <div className="bg-gradient-to-r from-cyan-300 to-sky-400 text-stone-800 w-1/6 space-y-4">
+      <p> Logo </p>
+      <nav>
+        <a className="block py-3 5 px-5"> </a>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
