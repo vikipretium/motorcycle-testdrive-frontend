c589918a6f972b9e8237e1c3c73780c877136fc3
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-19 10:32:10 +0200

    Add Routes and Nested Routes to the main App component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ModelsHome from './models/models';
+import AddReservations from './reservations/addreservation';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +10,14 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="models" element={<ModelsHome />} />
+          <Route path="addreservation" element={<AddReservations />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
 
