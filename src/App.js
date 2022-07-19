91c656557934896258b061a7695eb6955ecfa6e9
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-19 10:32:41 +0200

    Add an Outlet component to render extra components based on URL's passed by the user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Outlet } from 'react-router-dom';
 import NavBar from './side_navbar/Navbar';
 
 function App() {
@@ -6,10 +7,9 @@ function App() {
     <div className="relative min-h-screen md:flex">
       <NavBar />
       <div>
-        {' '}
         {/* Put all other content in this div  */}
         <h1>Motocycle Book</h1>
-        Other Content Goes Here
+        <Outlet />
       </div>
     </div>
   );
