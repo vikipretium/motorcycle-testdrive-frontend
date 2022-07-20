19439eebc72b33d46a1b625f6f1010537cc27e55
daudi13 <davouma13@gmail.com>
2022-07-20 20:49:55 +0300

    enclose App body in Routers tags

--- src/App.js
+++ src/App.js
@@ -1,49 +1,55 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, NavLink,
+} from 'react-router-dom';
 import './App.css';
 // import { Outlet } from 'react-router-dom';
 // import NavBar from './side_navbar/Navbar';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="relative min-h-screen md:flex">
-      {/* <NavBar /> Add conditional to show display navbar when user is logged in */}
-      <div>
-        {/* Put all other content in this div  */}
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to="/sign-in">
-              MotorHub
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sign-in">
-                    login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sign-up">
-                    sign up
-                  </Link>
-                </li>
-              </ul>
+    <Router>
+      <div className="relative min-h-screen md:flex">
+        {/* <NavBar /> Add conditional to show display navbar when user is logged in */}
+        <div>
+          {/* Put all other content in this div  */}
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <NavLink className="navbar-brand" to="/sign-in">
+                MotorHub
+              </NavLink>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/sign-in">
+                      login
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/sign-up">
+                      sign up
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route path="sign-in" element={<Login />} />
+                <Route path="sign-up" element={<SignUp />} />
+              </Routes>
             </div>
           </div>
-        </nav>
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/sign-in" element={<Login />} />
-              <Route path="/sign-in" element={<SignUp />} />
-            </Routes>
-          </div>
+          {/* <Outlet /> */}
         </div>
-        {/* <Outlet /> */}
       </div>
-    </div>
+    </Router>
   );
 }
 
