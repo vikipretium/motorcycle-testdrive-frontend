252c380c3c004aee4e682a7cb6d6130694d7de83
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-21 12:44:14 +0200

    Remove debugging code

--- src/reservations/userReservations.js
+++ src/reservations/userReservations.js
@@ -30,39 +30,3 @@ const UserReservations = () => {
 };
 
 export default UserReservations;
-
-// ActiveRecord::Schema[7.0].define(version: 2022_07_19_100658) do
-//   # These are extensions that must be enabled in order to support this database
-//   enable_extension "plpgsql"
-
-//   create_table "motorcycles", force: :cascade do |t|
-//     t.string "name"
-//     t.string "image"
-//     t.string "specification"
-//     t.integer "price"
-//     t.datetime "created_at", null: false
-//     t.datetime "updated_at", null: false
-//   end
-
-//   create_table "reservations", force: :cascade do |t|
-//     t.bigint "user_id", null: false
-//     t.bigint "motorcycle_id", null: false
-//     t.string "city"
-//     t.string "date"
-//     t.datetime "created_at", null: false
-//     t.datetime "updated_at", null: false
-//     t.index ["motorcycle_id"], name: "index_reservations_on_motorcycle_id"
-//     t.index ["user_id"], name: "index_reservations_on_user_id"
-//   end
-
-//   create_table "users", force: :cascade do |t|
-//     t.string "full_name"
-//     t.string "email"
-//     t.datetime "created_at", null: false
-//     t.datetime "updated_at", null: false
-//     t.string "password_digest"
-//   end
-
-//   add_foreign_key "reservations", "motorcycles"
-//   add_foreign_key "reservations", "users"
-// end
