5670f25fae4e2bda23f1f1d6c9afd32e3c022418
daudi13 <davouma13@gmail.com>
2022-07-21 11:26:57 +0300

    set up the useRef, useEffect and useState

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,48 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import {React, useRef, useEffect, useState } from 'react';
 
-const Login = () => (
-  <form className="auth-form">
-    <h3>Sign in</h3>
-    <div className="mb-3">
-      <label htmlFor="email-address">Email Address</label>
-      <input
-        type="email"
-        className="form-control"
-        placeholder="Enter your email"
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        className="form-control"
-        placeholder="Enter password"
-      />
-    </div>
-    <div className="mb-3">
-      <div className="custom-control custom-checkbox">
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd])
+  return (
+    <form className="auth-form">
+      <h3>Sign in</h3>
+      <div className="mb-3">
+        <label htmlFor="email-address">Email Address</label>
         <input
-          type="checkout"
+          type="email"
           className="form-control"
-          id="customcheck1"
+          placeholder="Enter your email"
         />
-        <label className="custom-control-label" htmlFor="customCheck1">
-          Remember me
-        </label>
       </div>
-    </div>
-    <div className="d-grid">
-      <button type="submit" className="btn btn-primary">
-        submit
-      </button>
-    </div>
-  </form>
-);
+      <div className="mb-3">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+        />
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          submit
+        </button>
+      </div>
+    </form>
+  );
+}
 
 export default Login;
