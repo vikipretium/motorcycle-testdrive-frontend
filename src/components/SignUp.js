1b7cbf8a183266d3f0134d68184e92cab7b79bfc
daudi13 <davouma13@gmail.com>
2022-07-21 23:08:04 +0300

    restructure component body

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,52 +3,79 @@ import {
   React, useRef, useEffect, useState,
 } from 'react';
 
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-const NAME_REGEX = /^[a-zA-Z]+$/;
-const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+const SignUp = () => {
+  const errRef = useRef();
 
 
-const SignUp = () => (
-  <form className="auth-form">
-    <h3>Sign up</h3>
-    <div className="mb-3">
-      <label htmlFor="full-name">Full name</label>
-      <input
-        type="text"
-        className="form-control"
-        placeholder="Enter your name"
-        id="full-name"
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="email-adress">Email address</label>
-      <input
-        type="email"
-        className="form-control"
-        placeholder="Enter email"
-        id="email-address"
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        className="form-control"
-        placeholder="Enter password"
-        id="password"
-      />
-    </div>
-    <div className="d-grid">
-      <button type="submit" className="btn btn-primary">
-        Sign up
-      </button>
-    </div>
-    <p className="forgot-password text-right">
-      Already have an account?
-      {' '}
-      <a href="/sign-in">sign in</a>
-    </p>
-  </form>
-);
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, email, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, pwd, email);
+    setUser('');
+    setPwd('');
+    setEmail('');
+    setSuccess(true);
+  };
+
+  return (
+    <>
+      {
+        success ? (
+          <section>
+            <h1>You are logged in</h1>
+            <br />
+            <p>
+              <a href="/">go to home</a>
+            </p>
+          </section>
+        ) : (
+          <section>
+            <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
+              {errMsg}
+            </p>
+            <form className="auth-form" onSubmit={handleSubmit}>
+              <h3>Sign up</h3>
+              <div className="mb-3">
+                <label htmlFor="full-name">Full name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter your name"
+                  id="full-name"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="email-adress">Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter email"
+                  id="email-address"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                  id="password"
+                />
+              </div>
+              <div className="d-grid">
+                <button type="submit" className="btn btn-primary">
+                  Sign up
+                </button>
+              </div>
+              <p className="forgot-password text-right">
+                Already have an account?
+                {' '}
+                <a href="/sign-in">sign in</a>
+              </p>
+            </form>
+  );
 
 export default SignUp;
