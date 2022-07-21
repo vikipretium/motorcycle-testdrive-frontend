0bbb6ad35a02bbd37a502d94e7beeda1af1b7ee0
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-22 01:53:32 +0200

    Add motorcycle fetching logic to the Motorcycle home page component

--- src/models/models.js
+++ src/models/models.js
@@ -1,5 +1,18 @@
-const ModelsHome = () => (
-  <h1>Models Home Page</h1>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMotorcycles } from '../redux/motorcylce/motorcylceSlice';
+
+const ModelsHome = () => {
+  const motorcycles = useSelector((state) => state.motorcycle);
+  const dispatch = useDispatch();
+  console.log(motorcycles);
+  useEffect(() => {
+    dispatch(fetchMotorcycles());
+  }, []);
+
+  return (
+    <h1>Models Home Page</h1>
+  );
+};
 
 export default ModelsHome;
