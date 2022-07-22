16a7cad9f686fbe8348ce8ac5e3906367d62bcd0
daudi13 <davouma13@gmail.com>
2022-07-22 15:14:59 +0300

    refactor code

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -57,61 +57,61 @@ const SignUp = () => {
         success ? (
           navigate('/homepage')
         ) : (
-          <section>
-            <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
-              {errMsg}
-            </p>
-            <form className="auth-form" onSubmit={handleSubmit}>
-              <h3>Sign up</h3>
-              <div className="mb-3">
-                <label htmlFor="full-name">Full name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  autoComplete="off"
-                  onChange={(e) => setUser(e.target.value)}
-                  value={user}
-                  placeholder="Enter your name"
-                  id="full-name"
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="email-adress">Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                  placeholder="Enter email"
-                  id="email-address"
-                  required
-                />
-              </div>
-              <div className="mb-3">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  onChange={(e) => setPwd(e.target.value)}
-                  value={pwd}
-                  placeholder="Enter password"
-                  id="password"
-                  required
-                />
-              </div>
-              <div className="d-grid">
-                <button type="submit" className="btn btn-primary">
-                  Sign up
-                </button>
-              </div>
-              <p className="forgot-password text-right">
-                Already have an account?
-                {' '}
-                <a href="/sign-in">sign in</a>
-              </p>
-            </form>
-          </section>
+                <section>
+                  <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
+                    {errMsg}
+                  </p>
+                  <form className="auth-form" onSubmit={handleSubmit}>
+                    <h3>Sign up</h3>
+                    <div className="mb-3">
+                      <label htmlFor="full-name">Full name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        autoComplete="off"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        placeholder="Enter your name"
+                        id="full-name"
+                        required
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label htmlFor="email-adress">Email address</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        placeholder="Enter email"
+                        id="email-address"
+                        required
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label htmlFor="password">Password</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        onChange={(e) => setPwd(e.target.value)}
+                        value={pwd}
+                        placeholder="Enter password"
+                        id="password"
+                        required
+                      />
+                    </div>
+                    <div className="d-grid">
+                      <button type="submit" className="btn btn-primary">
+                        Sign up
+                      </button>
+                    </div>
+                    <p className="forgot-password text-right">
+                      Already have an account?
+                      {' '}
+                      <a href="/sign-in">sign in</a>
+                    </p>
+                  </form>
+                </section>
         )
 }
     </>
