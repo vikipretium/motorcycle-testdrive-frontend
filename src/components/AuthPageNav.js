88cc74c5e22bad9f1c6bce2f0c27da6eb76069d7
daudi13 <davouma13@gmail.com>
2022-07-22 15:41:52 +0300

    create AuthPageNav component

---
+++ src/components/AuthPageNav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const AuthPageNav = () => (
+  <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+    <div className="container">
+      <NavLink className="navbar-brand" to="/sign-in">
+        MotorHub
+      </NavLink>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/sign-in">
+              login
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/sign-up">
+              sign up
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default AuthPageNav;
