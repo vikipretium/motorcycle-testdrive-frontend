0fcbb8654413f27039eb7c9626ec1b5761959152
daudi13 <davouma13@gmail.com>
2022-07-22 15:15:50 +0300

    Add navbar

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -57,6 +57,31 @@ const SignUp = () => {
         success ? (
           navigate('/homepage')
         ) : (
+          <div className="wrapper">
+            {/* Put all other content in this div  */}
+            <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+              <div className="container">
+                <NavLink className="navbar-brand" to="/sign-in">
+                  MotorHub
+                </NavLink>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                  <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/sign-in">
+                        login
+                      </NavLink>
+                    </li>
+                    <li className="nav-item">
+                      <NavLink className="nav-link" to="/sign-up">
+                        sign up
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+            <div className="auth-wrapper">
+              <div className="auth-inner">
                 <section>
                   <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live="assertive">
                     {errMsg}
