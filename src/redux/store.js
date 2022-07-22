dc4e403dd1f7d03c502b4040c90c6307c89f3d45
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-22 03:38:26 +0200

    Add reservationReducer to the redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import motorcycleReducer from './motorcylce/motorcylceSlice';
+import reservationReducer from './reservation/reservationSlice'
 
 export default configureStore({
   reducer: {
     motorcycle: motorcycleReducer,
+    reservation: reservationReducer,
   },
   // Middleware to show Redux Transactions: Please remove when we push to production
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
