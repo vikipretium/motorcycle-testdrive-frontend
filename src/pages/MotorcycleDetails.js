0de43d3a2a906117743b54d1398c9aedee53ccfd
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-26 16:15:42 +0545

    Create function and use useState

--- src/pages/MotorcycleDetails.js
+++ src/pages/MotorcycleDetails.js
@@ -1,2 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
+const MotorcycleDetail = () => {
+  const [bike, setbike] = useState({});
+  const { motorcycleId } = useParams();
+  const fetchMotorcycle = async () => {
+    const Url = await fetch(`http://localhost:3000/api/v1/motorcycles/${motorcycleId}/`);
+    const data = await Url.json();
+    setbike(data.data);
+  };
