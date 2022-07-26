9f4bf7eec475cd9fe57b5f267f0fd9a7e62213f5
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-26 16:19:42 +0545

    Style the page and use it

--- src/pages/MotorcycleDetails.js
+++ src/pages/MotorcycleDetails.js
@@ -13,3 +13,29 @@ const MotorcycleDetail = () => {
   useEffect(() => {
     fetchMotorcycle();
   }, []);
+
+  return (
+    <div className="w-full flex items-center p-4">
+      <div className="w-3/5 p-4">
+        <img src={bike?.image} alt="motorcycleImage" className="image pl-16" />
+      </div>
+      <div className="w-96">
+        <h2 className="font-bold text-3xl">{bike?.name}</h2>
+        <div className="my-3">
+          <p className="bg-slate-300 my-2 p-2">
+            <span className="font-medium">
+              Total amount: $
+              {bike?.price}
+            </span>
+          </p>
+          <p className="font-medium my-2 p-2">Fuel: Petrol</p>
+          <p className="font-medium bg-slate-300 my-2 p-2">Location: Jhapa,Nepal</p>
+          <p className="font-medium p-2">Contact: +9779824955067</p>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full my-2">Reservation</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MotorcycleDetail;
