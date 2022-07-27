ee89351666c6b0591706ff710785675eaaac326d
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-27 08:54:08 +0545

    Add style for buttons

--- src/pages/MotorcycleDetails.js
+++ src/pages/MotorcycleDetails.js
@@ -29,7 +29,7 @@ const MotorcycleDetail = () => {
             <p className="font-medium my-2 p-2">Fuel: Petrol</p>
             <p className="font-medium bg-slate-300 my-2 p-2">Location: Jhapa,Nepal</p>
             <p className="font-medium p-2">Contact: +9779824955067</p>
-            <button type="submit" className="buttons bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full my-2">Reservation</button>
+            <button type="submit" className="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full my-2">Reservation</button>
           </div>
         </div>
       </div>
