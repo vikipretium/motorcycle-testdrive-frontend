695152848714ef5995fce6ce9a701b4428c5a2fd
vikipretium <vighneshpasupathy@gmail.com>
2022-07-28 14:27:08 +0530

    change axiox link and configjs

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 /* eslint-disable import/prefer-default-export */
-export const API_BASE_URL = 'http://localhost:3001/api/v1';
+export const API_BASE_URL = 'https://enigmatic-brushlands-79839.herokuapp.com/api/v1';
--- src/context/api/axios.js
+++ src/context/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://enigmatic-brushlands-79839.herokuapp.com',
 });
