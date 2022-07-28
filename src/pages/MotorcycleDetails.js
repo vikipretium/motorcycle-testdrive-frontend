15cb5017de0718873e1d05c5d19358df1497a2a2
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-07-28 12:47:38 +0545

    Provide link for reservation

--- src/pages/MotorcycleDetails.js
+++ src/pages/MotorcycleDetails.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import NavBar from '../side_navbar/Navbar';
 
@@ -25,7 +25,7 @@ const MotorcycleDetail = () => {
             <p className="font-medium my-2 p-2">Fuel: Petrol</p>
             <p className="font-medium bg-slate-300 my-2 p-2">Location: Jhapa,Nepal</p>
             <p className="font-medium p-2">Contact: +9779824955067</p>
-            <button type="submit" className="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full my-2">Reservation</button>
+            <Link className="button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full my-2" to="addreservation">Reservation</Link>
           </div>
         </div>
       </div>
